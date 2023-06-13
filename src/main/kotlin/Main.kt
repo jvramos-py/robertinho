96a63ca6fc76ed3a42376b3e3f263df62556ebfc
josev.moraes <josev.moraes@telefonica.com>
2023-06-12 22:25:15 -0300

    Criando mais funções para o bot

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,12 +8,36 @@ import io.github.cdimascio.dotenv.dotenv
 val dotenv = dotenv()
 suspend fun main() {
     val kord = Kord(dotenv["TOKEN"])
-
+    val suggestions: MutableList<String> = mutableListOf()
     kord.on<MessageCreateEvent> {
         if (message.author?.isBot != false) return@on
-        if (message.content != "!ping") return@on
-        
-        message.channel.createMessage("pong!")
+        when (message.content) {
+            "!ping" -> message.channel.createMessage("pong!")
+            "!pin" -> {
+                message.referencedMessage!!.pin("pinned by Robertinho.")
+                message.channel.createMessage("Mensagem fixada!")
+            }
+
+            "!help" -> message.channel.createMessage(
+                "Olá! Aqui estão algumas coisas que posso fazer: \n" + "!pin: Irei fixar a mensagem referenciada no canal de texto citada. \n" + "!help: Lista de funções disponíveis. \n" + "!addSuggestion {sugestão}: Caso queira adicionar uma sugestão de futuras implementações de funcionalidades do bot :)."
+            )
+
+
+            "!getSuggestions" -> {
+                if (message.author!!.username == "UnosTristes") message.channel.createMessage("Aqui está: \n" + suggestions.let { return@let "$it \n" })
+            }
+
+            else -> return@on
+        }
+
+    }
+
+    kord.on<MessageCreateEvent> {
+        if (message.content == "!teste") message.channel.createMessage(message.author!!.username)
+        if (message.content.startsWith("!addSuggestion")) {
+            message.channel.createMessage("Obrigado pela sugestão! :)")
+            suggestions.add(index = suggestions.lastIndex + 1, element = message.content.removePrefix("!addSuggestion"))
+        }
     }
 
     kord.login {
